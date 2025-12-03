ad093ddbda5d06f3860ecaec5d68a786bab4b795
Andersonsimba <anderson.simba@ister.edu.ec>
2025-12-02 23:20:59 -0500

    Tabla salas

--- APP_CINE/app/Models/Pelicula.php
+++ APP_CINE/app/Models/Pelicula.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pelicula extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['titulo','sinopsis','duracion','poster_url','tmdb_id'];
+
+    public function generos()
+    {
+        return $this->belongsToMany(Genero::class, 'pelicula_genero');
+    }
+
+    public function funciones()
+    {
+        return $this->hasMany(Funcion::class);
+    }
 }
