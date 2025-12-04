573e954e736cbfbbaba3e52723de16c9180b24a5
Andersonsimba <anderson.simba@ister.edu.ec>
2025-12-04 09:37:57 -0500

    Api

--- APP_CINE/app/Http/Controllers/Api/GeneroController.php
+++ APP_CINE/app/Http/Controllers/Api/GeneroController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Genero;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 class GeneroController extends Controller
 {
     public function index()
--- APP_CINE/app/Http/Controllers/Api/ReservaController.php
+++ APP_CINE/app/Http/Controllers/Api/ReservaController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Reserva;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class ReservaController extends Controller
 {
